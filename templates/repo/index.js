f9ce6f2bad219816009df9a82a69d39bf98c96c4
Alexander Abramov <abramov231@gmail.com>
2020-08-08 11:41:06 +0300

    конфиг редактора

--- templates/repo/index.js
+++ templates/repo/index.js
@@ -1,12 +1,11 @@
 
 exports.getConfig = function getConfig (args) {
+    const { name } = args;
     return {
         entities: [
-            { input: './package.json.ejs', output: '@/package.json' },
-            { input: './.gitignore.ejs', output: '@/.gitignore' },
+            { input: './package.json.ejs', output: `@/${name}/package.json` },
+            { input: './.gitignore', output: `@/${name}/.gitignore` },
+            { input: './.editorconfig', output: `@/${name}/.editorconfig` },
         ],
-        data () {
-            return args;
-        },
     };
 };
