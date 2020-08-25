2a96677fdef29752a0d232e90a0b955dde5fdf9f
Alexander Abramov <abramov231@gmail.com>
2020-08-25 20:26:58 +0300

    draft crud generator

---
+++ templates/crud/index.js
@@ -0,0 +1,10 @@
+
+exports.getConfig = function getConfig (args) {
+    const { name } = args;
+    return {
+        entities: [
+            { input: './model.ts.ejs', output: `@/src/models/${name.snakeCase}.ts` },
+            { input: './model.test.ts.ejs', output: `@/src/models/${name.snakeCase}.test.ts` },
+        ],
+    };
+};
