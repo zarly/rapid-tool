b3591fd3047ada9ad3f642c4ad8bbacd87d13e51
Alexander Abramov <abramov231@gmail.com>
2020-09-19 00:35:46 +0300

    fix-apps

--- templates/web-svelte/index.js
+++ templates/web-svelte/index.js
@@ -8,6 +8,7 @@ exports.getConfig = function getConfig (args) {
             { cmd: `pwd` },
             { cmd: `mkdir -p ${dir}` },
             { cmd: `cp -R ${path.resolve(__dirname, 'svelte')}/. ./${dir}` },
+            { input: './.gitignore.ejs', output: `@/${dir}/.gitignore` },
             { cmd: `tar -xvf ${path.resolve(__dirname, 'deps.tar.gz')} -C ./${dir}` },
             { cmd: `cd ${dir} && git init && git add . && git commit -m init && cd -` },
         ],
